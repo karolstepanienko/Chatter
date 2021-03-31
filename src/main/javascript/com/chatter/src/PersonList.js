3e6f74f6d4cb1f6cbd82cb33da5b437c96ccb2d2
Karol Stepanienko <k.stepanienko@stud.elka.pw.edu.pl>
2021-03-31 14:11:48 +0200

    Added simple React Hello World programm.

---
+++ src/main/javascript/com/chatter/src/PersonList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import axios from 'axios';
+
+export default class PersonList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      persons: [],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+      Hello World!
+      </div>
+    )
+  }
+}
---
+++ src/main/javascript/com/chatter/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './PersonList.js'
+import PersonList from './PersonList';
+
+ReactDOM.render(
+  <PersonList />,
+  document.getElementById('root')
+);
