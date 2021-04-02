fe4fb096221683587726cc22ed75d1ca922fc702
Karol Stepanienko <k.stepanienko@stud.elka.pw.edu.pl>
2021-04-02 15:04:13 +0200

    Prepare for merge.

--- src/main/java/com/chatter/controllers/helloController/HelloController.java
+++ src/main/java/com/chatter/controllers/helloController/HelloController.java
@@ -12,11 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
-	@RequestMapping("/")
-	public void index(HttpServletResponse response) throws IOException {
-		response.sendRedirect("html/index.html");
-	}
-
   @RequestMapping("/hello")
   public String folder() {
     return "Hello world!";
