092723883269013d38c6ccbfebfc56814cc2951b
Karol Stepanienko <k.stepanienko@stud.elka.pw.edu.pl>
2021-04-03 16:03:15 +0200

    Working account registration with validation for login and email.

--- src/main/js/com/chatter/src/Components/Pages/Account/Register.js
+++ src/main/js/com/chatter/src/Components/Pages/Account/Register.js
@@ -1,76 +1,75 @@
 import React from 'react';
 import axios from 'axios';
 
+//check useEffect
+
 // Internal imports:
 import '../../../css/Pages/Account/Register.css';
-import { link }from '../../../Constants/Constants';
+import { link, validEmailRegex }from '../../../Constants/Constants';
 
 const linkRegister = `${link}/account/register`;
+
 
 export default class Register extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      id: null,
-      name: '',
-      login: '',
-      email: '',
-    },
-    // Methods:
-    this.handleLoginChange = this.handleLoginChange.bind(this);
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handleRegister = this.handleRegister.bind(this);
-
     this.loginTaken = false;
     this.emailTaken = false;
+    this.malformedLogin = false;
+    this.user = {
+      id: null,
+      login: '',
+      login: '',
+      email: '',
+    }
   }
 
-  handleRegister = event => {
-    event.preventDefault();
-
-    this.checkLogin();
-    this.checkEmail();
-    
-    console.log(this.loginTaken);
-    console.log(this.state.login);
-    console.log(this.emailTaken);
-    console.log(this.state.email);
-
-    if(this.loginTaken==false && this.emailTaken==false) {
-      axios.post(`${linkRegister}/add/user`, this.state);
+  handleRegister(evt) {
+    evt.preventDefault();
+    if(this.loginTaken && this.emailTaken) alert("Login and email already taken.");
+    else if(this.loginTaken) alert("Login is already taken.");
+    else if(this.emailTaken) alert("Email is already taken.");
+    else if(
+      this.loginTaken==false
+      && this.emailTaken==false
+      && this.malformedLogin==false
+      && this.malformedEmail==false) {
+      axios.post(`${linkRegister}/add/user`, this.user);
     } 
-    if(this.loginTaken) alert("Login is already taken.");
-    if(this.emailTaken) alert("Email is already taken.");
   }
 
-  checkLogin() {
-    axios.post(`${linkRegister}/check/login`, this.state)
-    .then(res => {
-      if(res.data) {
-        this.loginTaken = true;
-      } else this.loginTaken = false;
-    });
+  checkLoginAvailable() {
+    axios.post(`${linkRegister}/check/login`, this.user)
+    .then(res => { this.loginTaken = res.data });
   }
 
-  checkEmail() {
-    axios.post(`${linkRegister}/check/email`, this.state)
-    .then(res => {
-      if(res.data) {
-        this.emailTaken = true;
-      } else this.emailTaken = false;
-    });
+  checkEmailAvailable() {
+    axios.post(`${linkRegister}/check/email`, this.user)
+    .then(res => { this.emailTaken = res.data });
   }
 
-  handleLoginChange = event => {
-    event.preventDefault();
-    this.setState({login: event.target.value});
-    this.checkLogin();
+  checkLoginFormat() {
+    if(this.user.login.length > 3 && this.user.login.length < 20) this.malformedLogin = false;
+    else this.malformedLogin = true;
   }
 
-  handleEmailChange = event => {
-    event.preventDefault();
-    this.setState({email: event.target.value});
-    this.checkEmail();
+  checkEmailFormat() {
+    if(validEmailRegex.test(this.user.email)) this.malformedEmail = false;
+    else this.malformedEmail = true;
+  }
+
+  handleLoginChange(evt) {
+    evt.preventDefault();
+    this.user.login = evt.target.value;
+    this.checkLoginFormat();
+    if(this.malformedLogin==false) this.checkLoginAvailable();
+  }
+
+  handleEmailChange(evt) {
+    evt.preventDefault();
+    this.user.email = evt.target.value;
+    this.checkEmailFormat();
+    if(this.malformedEmail==false) this.checkEmailAvailable();
   }
 
   render() {
@@ -78,29 +77,33 @@ export default class Register extends React.Component {
       <div className="register-page">
         <h2 className="register-title">Register to chatter.</h2>
         <form className="form">
-          <label className="login">Login:</label>
+          <label className="login" for="login">Login:</label>
           <input 
             className="login"
             type="text"
             name="login"
             placeholder="Your login"
-            value={this.state.login}
-            onChange={this.handleLoginChange}></input>
+            minLength="3"
+            maxLength="20"
+            onChange={evt => this.handleLoginChange(evt)}>
+          </input>
 
-          <label className="email">Email:</label>
+          <label className="email" for="email">Email:</label>
           <input
             className="email"
-            type="text"
+            type="email"
             name="email"
             placeholder="Your email"
-            value={this.state.email}
-            onChange={this.handleEmailChange}></input>
+            minLength="3"
+            onChange={evt => this.handleEmailChange(evt)}>
+          </input>
           
           <input 
-          className="button"
-          type="submit"
-          value="Register"
-          onClick={this.handleRegister}></input>
+            className="button"
+            type="submit"
+            value="Register"
+            onClick={evt => this.handleRegister(evt)}>
+          </input>
         </form>
       </div>
     );
--- src/main/js/com/chatter/src/Constants/Constants.js
+++ src/main/js/com/chatter/src/Constants/Constants.js
@@ -1,1 +1,3 @@
 export const link = 'http://localhost:8080/api/';
+
+export const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
