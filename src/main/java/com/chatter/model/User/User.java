378f4f853935421a362a2da49002020f6898f12a
Karol Stepanienko <k.stepanienko@stud.elka.pw.edu.pl>
2021-05-10 18:06:27 +0200

    Added lombok support

--- src/main/java/com/chatter/model/User/User.java
+++ src/main/java/com/chatter/model/User/User.java
@@ -6,8 +6,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 // This tells Hibernate to make a table in database out of this class
 // Hibernate automatically translates the entity into a table.
+@Data
 @Entity
 public class User {
 
@@ -28,65 +31,4 @@ public class User {
     this.email = email;
     this.passwordHash = passwordHash;
   }
-
-  // Getters
-  public Integer getId() {
-    return this.id;
-  }
-
-  public String getUserName() {
-    return this.userName;
-  }
-
-  public String getLogin() {
-    return this.login;
-  }
-
-  public String getEmail() {
-    return this.email;
-  }
-
-  public String getPasswordHash() {
-    return this.passwordHash;
-  }
-
-  public String getRole() {
-    return this.role;
-  }
-
-
-  // Setters
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public void setUserName(String userName) {
-    this.userName = userName;
-  }
-
-  public void setLogin(String login) {
-    this.login= login;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public void setPasswordHash(String passwordHash) {
-    this.passwordHash = passwordHash;
-  }
-
-  public void setRole(String role) {
-    this.role = role;
-  }
-
-  public String toString() {
-    return "Id: " + this.id + ", " +
-    "Username: " + this.userName + ", " +
-    "Login: " + this.login + ", " +
-    "Email: " + this.email + ", " + 
-    "PasswordHash: " + this.passwordHash + ", " +
-    "Role: " + this.role;
-  }
-  
 }
--- src/main/java/com/chatter/model/User/UserDTO.java
+++ src/main/java/com/chatter/model/User/UserDTO.java
@@ -1,9 +1,12 @@
 package com.chatter.model.User;
 
+import lombok.Data;
+
 // Class DTO - User Data Transfer Object
 // Only difference from User is that
 // it has password in plain text
 
+@Data
 public class UserDTO {
 
   private Integer id;
@@ -22,67 +25,5 @@ public class UserDTO {
     this.email = email;
     this.password = password;
     this.role = role;
-  }
-
-  // Getters
-  public Integer getId() {
-    return this.id;
-  }
-
-  public String getUserName() {
-    return this.userName;
-  }
-
-  public String getLogin() {
-    return this.login;
-  }
-
-  public String getEmail() {
-    return this.email;
-  }
-
-  public String getPassword() {
-    return this.password;
-  }
-
-  public String getRole() {
-    return this.role;
-  }
-
-
-  // Setters
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public void setName(String userName) {
-    this.userName = userName;
-  }
-
-  public void setLogin(String login) {
-    this.login= login;
-  }
-
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public void setRole(String role) {
-    this.role = role;
-  }
-
-  public String toString() {
-    return "Id: " + this.id + ", " +
-    "Username: " + this.userName + ", " +
-    "Login: " + this.login + ", " +
-    "Email: " + this.email + ", " + 
-    "Password: " + this.password + ", " +
-    "Role: " + this.role;
-  }
-  
+  }  
 }
