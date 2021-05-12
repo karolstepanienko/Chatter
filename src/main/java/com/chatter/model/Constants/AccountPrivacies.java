da297904497ec6e7dc5a67fac72b48ecb4bd5a0a
Karol Stepanienko <k.stepanienko@stud.elka.pw.edu.pl>
2021-05-13 01:31:58 +0200

    Added privacy settings constants.

--- src/main/java/com/chatter/model/Constants/AccountPrivacies.java
+++ src/main/java/com/chatter/model/Constants/AccountPrivacies.java
@@ -1,5 +1,35 @@
 package com.chatter.model.Constants;
 
-public final class AccountPrivacies {
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.Data;
+
+@Data
+public class AccountPrivacies {
+  private static String publicAccess = "PUBLIC";
+  private static String friendOnlyAccess = "FRIEND_ONLY";
+  private static String privateAccess = "PRIVATE";
+
+  public static String getPublicAccess() {
+    return publicAccess;
+  }
+
+  public static String getFriendOnlyAccess() {
+    return friendOnlyAccess;
+  }
+  
+  public static String getPrivateAccess() {
+    return privateAccess;
+  }
+
+  public static Map<String, String> getAllAccess() {
+    Map<String, String> map = new HashMap<String, String>();
+    map.put("publicAccess", publicAccess);
+    map.put("friendOnlyAccess", friendOnlyAccess);
+    map.put("privateAccess", privateAccess);
+    return map;
+  }
+
   
 }
