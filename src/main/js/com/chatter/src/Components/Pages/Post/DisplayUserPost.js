18d89180ede1576652f32a7c07102c4d093428c5
Karol Stepanienko <k.stepanienko@stud.elka.pw.edu.pl>
2021-05-16 19:54:19 +0200

    [REFRACTOR] Moved user post display comonent to a new file.

---
+++ src/main/js/com/chatter/src/Components/Pages/Post/DisplayUserPost.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+import { link } from '../../../Constants/Constants';
+
+
+const DisplayUserPost = (props) => {
+
+  const getLink = () => {
+    var link_to_user = `/user/${props.userName}`
+    return link_to_user;
+  }
+
+  const handlePostDelete = () => {
+    if (window.confirm("Are you sure, you want to delete this post?")) {
+      // Deletes by path variable
+      axios.post(`${link}/post/delete/by/Id/${props.post.id}`).then(
+        res => props.handlePostRemove(props.post.id)
+      ).catch( err => console.log(err))  
+    }
+  }
+
+  const handlePrivacyChange = () => {
+    axios.post(`${link}/post/update/privacy/${props.post.id}`).then(
+      res => alert("Post privacy setting changed.")
+    ).catch(err => console.log(err))
+  }
+
+  return (
+    <div className="post">
+      <span className="post-title">Post:</span>
+      <div className="creator">
+        <span>Autor: </span>
+        <Link to={getLink()} userName={props.userName}>{props.userName}</Link>
+      </div>
+      <div className="post-text">
+        {(props.post.text)}
+      </div>
+      <button className="button-changePrivacy"
+        onClick={handlePrivacyChange}>
+          Change post privacy
+      </button>
+      <button className="button-delete"
+        onClick={handlePostDelete}>
+        Delete post
+      </button>
+    </div>
+  )
+}
+
+export default DisplayUserPost;
